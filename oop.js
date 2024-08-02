2b6be4d6e236ad7a0e1f39153a240008af6d4b04
kehua <1607080665@qq.com>
2024-08-02 14:40:45 +0800

    class

--- oop.js
+++ oop.js
@@ -1,72 +1,58 @@
 
   // 表示一个二维向量
-  function Vector(x, y) {
-    this.x = x
-    this.y = y
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-  Vector.prototype.plus = function (v) {
-    var x = this.x + v.x
-    var y = this.y + v.y
-    return new Vector(x, y)
+
+  plus(v) {
+    return new Vector(this.x + v.x, this.y + v.y);
   }
-  Vector.prototype.minus = function (v) {
-    var x = this.x - v.x
-    var y = this.y - v.y
-    return new Vector(x, y)
+
+  minus(v) {
+    return new Vector(this.x - v.x, this.y - v.y);
   }
-  // Vector.prototype.length = function () {
-  //   return Math.sqrt(this.x * this.x + this.y * this.y)
-  // }
-  Object.defineProperty(Vector.prototype, 'length', {
-    get: function() {
-      return Math.sqrt(this.x * this.x + this.y * this.y)
-    }
-  })
 
-  var v1 = new Vector(1, 2)
-  var v2 = new Vector(3, -4)
-
-  // var v3 = v1.plus(v2)
-  // var v4 = v2.minus(v1)
-
-  // var l = v4.length()
-
-
-
-  // 表示一个复数(complex number)
-  function Complex(real, imag) {
-    this.real = real
-    this.imag = imag
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
   }
-  Complex.prototype.plus = function(c) {
-    var real = this.real + c.real
-    var imag = this.imag + c.imag
-    return new Complex(real, imag)
-  }
-  Complex.prototype.minus = function(c) {
-    var real = this.real - c.real
-    var imag = this.imag - c.imag
-    return new Complex(real, imag)
-  }
-  Complex.prototype.multiple = function(c) {
-    var real = this.real * c.real - this.imag * c.imag
-    var imag = this.real * c.imag + this.imag * c.real
-    return new Complex(real, imag)
-  }
-  Complex.prototype.div = function(c) {
-    var helper = new Complex(c.real, -c.imag)
-    var fenmu = c.multiple(helper).real
-    var fenzi = this.multiple(helper)
+}
 
-    var real = fenzi.real / fenmu
-    var imag = fenzi.imag / fenmu
-
-    return new Complex(real, imag)
+class Complex {
+  constructor(real, imag) {
+    this.real = real;
+    this.imag = imag;
   }
-  Complex.prototype.toString = function() {
+
+  plus(c) {
+    return new Complex(this.real + c.real, this.imag + c.imag);
+  }
+
+  minus(c) {
+    return new Complex(this.real - c.real, this.imag - c.imag);
+  }
+
+  multiple(c) {
+    const real = this.real * c.real - this.imag * c.imag;
+    const imag = this.real * c.imag + this.imag * c.real;
+    return new Complex(real, imag);
+  }
+
+  div(c) {
+    const helper = new Complex(c.real, -c.imag);
+    const denominator = c.multiple(helper).real;
+    const numerator = this.multiple(helper);
+    const real = numerator.real / denominator;
+    const imag = numerator.imag / denominator;
+
+    return new Complex(real, imag);
+  }
+
+  toString() {
     return '' + this.real + (this.imag > 0 ? "+" : '') + this.imag + 'i'
   }
-
+}
   // var c1 = new Complex(4, 5)
   // var c2 = new Complex(1, -2)
 
@@ -94,6 +80,107 @@
   // =================================================================
 
   // 表示一个单向链表
+  class LinkedList{
+    constructor(){
+      this._head=null
+      this._length=0
+    }
+    Node(idx){
+      this.idx=idx
+      this.next=null
+    }
+    at(idx){
+      var p=this._head
+      while(idx > 0 && p){
+        p=p.next
+        idx--
+      }
+      if(p){
+        return p.val
+      }else{
+        return undefined
+      }
+    }
+    set(idx, val){
+      var p =this._head
+      while(idx>0 && p){
+        p=p.next
+        idx--
+      }
+      if(p){
+        p.val =val
+      }
+    }
+    append(val){
+      var node  ={
+        val:val,
+        next:null,
+      }
+      this._length++
+      if(this._head == null){
+        this._head = node
+        return this
+      }
+      var p = this._head
+      while(p.next){
+        p=p.next
+      }
+      p.next = node
+      return this
+    }
+    pop(){
+      if(this._head == null){
+        return undefined
+      }
+      this._length--
+      if(this._head.next==null){
+        var result = this._head.val
+        this._head = null
+        return result
+      }
+      var p= this._head
+      while(p.next.next){
+        p=p.next
+      }
+      var result =p.next.val
+      p.next = null
+      return result
+    }
+    prepend(val){
+      var node = {
+        val:val,
+        next:this._head
+      }
+      this._length++
+      this._head = node
+      return this
+    }
+    shift(){
+      if(this._head == null){
+        return undefined
+      }
+      this._length--
+      var result = this._head.val
+      this._head = this._head.next
+      return result
+    }
+    toArray(){
+      var result = []
+      var p = this._head
+      while(p){
+        result.push(p.val)
+        p = p.next
+      }
+      return result
+    }
+    toString(){
+      return this.toArray().join('->')
+    }
+    get size() {
+      return this._length
+    }
+  }
+  
   function LinkedList() {
    this._head=null
    this._length=0
@@ -205,6 +292,42 @@
 
   // 表示一个集合（集合中元素没有序，但不能重复）
   // 构造函数可选的可以传入集合中的初始值，但会被去重后存放
+  class MySet{
+    constructor(initalValues = []){
+      this._elements = []
+      for(var val of initalValues){
+        this.add(val)
+      }
+    }
+    add(value){
+      if(!this._elements.includes(value)){
+        this._elements.push(value)
+      }
+      return this
+    }
+    delete(value){
+      var idx= this._elements.indexOf(value)
+      if(idx >= 0){
+        this._elements.splice(idx,1)
+      }
+      return this
+    }
+    get size() {
+      return this._length
+    }
+    clear(){
+      this._elements=[]
+      return this
+    }
+    has(value){
+      return this._elements.includes(value);
+    }
+    forEach(){
+      for (let value of this._elements) {
+        func(value)
+      }
+    }
+  }
   function MySet(initalValues = []) {
     this._elements = []
     for(var val of initalValues){
@@ -269,6 +392,63 @@
 
   // 表示一个映射
   // 这个映射中，可以把任何值映射到任何值，映射的key不限于字符串
+  class MyMap{
+    constructor(initPairs = []){
+      this._pairs = []
+      for(var pair of initPairs){
+        var key = pair[0]
+        var val = pair[1]
+        this.set(key,val)
+      }
+    }
+    set(key,val){
+      for(var i=0;i<this._pairs.length;i+=2){
+        if(this._pairs[i] === key){
+          this._pairs[i+1]=val
+          return this
+        }
+        }
+        this._pairs.push(key,val)
+        return this
+    }
+    get(key){
+      for(var i = 0; i<this._pairs.length;i+=2){
+        if(this._pairs[i] === key){
+          return this._pairs[i+1]
+        }
+      }
+      return undefined
+    }
+    has(key){
+      for(var i = 0;i<this._elements.length;i+=2){
+        if(this._pairs[i]===key){
+          return true
+        }
+      }
+      return false
+    }
+    delete(key){
+      for(var i=0;i<this._pairs.length;i+=2){
+        if(this._pairs[i] === key){
+          this._pairs.splice(i,2)
+          return true
+        }
+      }
+      return false
+    }
+    clear(){
+      this._pairs=[]
+      return this
+    }
+    get size(){
+      return this._pairs.length/2
+    }
+    forEach(iterator) {
+      for(var i = 0;i<this._elements.length;i+=2){
+        iterator(this._pairs[i+1],this._pairs[i])
+      }
+    }
+  }
   function MyMap(initPairs = []) {
     this._pairs = []
     for(var pair of initPairs){
@@ -336,6 +516,23 @@
 
 
   // 表示一个栈：即后进先出，先进后出
+  class Stack{
+    constructor(){
+      this._elements=[]
+    }
+    push(val){
+      this._elements.push(val)
+    }
+    pop(){
+      return this._elements.pop()
+    }
+    peek(){
+      return this._elements[this._elements.length - 1]
+    }
+    get size(){
+      return this._elements.length
+    }
+  }
   function Stack() {
     this._elements=[]
   }
@@ -371,6 +568,47 @@
 
 
   // 表示一个队列：即先进先出，后进后出
+  class Queue{
+    constructor(){
+      this._head = null
+    this._tail = null
+    this._length = 0
+    }
+    add(val){
+      var node={
+        val:val,
+        next:null,
+      }
+      this._length++
+      if(this._head == null){
+        this._head = this._tail = node
+        return this
+      }
+      this._tail.next=node
+      this._tail = node
+      return this
+    }
+    pop(val){
+      if(this._head == null){
+        return undefined
+      }
+      this._length--
+      if(this._head == this._tail){
+        var result=this._haed.val
+        this._head = this.tail = null
+        return result
+      }
+      var result = this._head.val
+      this._head = this._head.next
+      return result
+    }
+    peek(){
+      return this._head.val
+    }
+    get size(){
+      return this._length
+    }
+  }
   function Queue() {
     this._head = null
     this._tail = null
